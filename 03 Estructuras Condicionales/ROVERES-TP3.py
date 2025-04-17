1c951018e3fc1f127d4ae469110aa0e189aa1fa8
IgnacioRoveres <ignaciovroveres10@gmail.com>
2025-04-17 16:01:32 -0300

    ejercicios 2 y 3 del tp 3 hechos

---
+++ 03 Estructuras Condicionales/ROVERES-TP3.py
@@ -0,0 +1,11 @@
+# ejercicio 1
+edad = int(input("Ingrese su edad: "))
+if edad > 18:
+    print("Es mayor de edad")
+
+# ejercio 2
+nota = int(input("Ingrese su nota: "))
+if nota >= 6:
+    print("Aprobado")
+else:
+    print("Desaprobado")
